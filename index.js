71863891ac7403c5199a20d33fa23c9566e865ff
Joshi <126943242+Joshi1531@users.noreply.github.com>
2024-10-10 08:26:56 +0000

    Sync Changes

--- index.js
+++ index.js
@@ -15,24 +15,29 @@ function load_player_inps(num) {
         //const element = document.getElementsByClassName("plInp")[index];
         //document.removeChild(element);
     //}
-   for (let index = 1; index < parseInt(num) + 1; index++) {
-        //var centerDiv = document.createElement("div");
-        //centerDiv.classList.add("centerDiv")
-        //centerDiv.id = "pl" + index.toString() + "wrap"
-        //document.getElementById("setupDiv").append(centerDiv);
+    document.body.style.backgroundColor = "blue";
+    for (let index = 1; index < parseInt(num) + 1; index++) {
+        document.body.style.backgroundColor = "green";
+        var centerDiv = document.createElement("div");
+        centerDiv.classList.add("centerDiv")
+        centerDiv.id = "pl" + index.toString() + "wrap"
+        document.body.style.backgroundColor = "yellow";
+        document.getElementById("setupDiv").append(centerDiv);
+        document.body.style.backgroundColor = "gray";
         var newElem = document.createElement("input");
         newElem.type = "text";
         newElem.id = "namePl" + index.toString();
         newElem.classList.add("plInp");
         newElem.placeholder = "Player " + index.toString() + " Name";
-        //centerDiv.append(newElem);
+        centerDiv.append(newElem);
         var nwElem = document.createElement("input");
         nwElem.type = "number";
         nwElem.id = "cardsPl" + index.toString();
         nwElem.classList.add("plCardInp");
         nwElem.placeholder = "Cards";
-        //centerDiv.append(nwElem);
+        centerDiv.append(nwElem);
     }
+    document.body.style.backgroundColor = "red";
     document.getElementById("namePl1").value = "You";
     //Submit Button
     const submBtn = document.createElement("button")
